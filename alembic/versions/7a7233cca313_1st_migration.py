fc301225fb87c89601395373eb163da487778792
tantsiura <97056496+tantsiura@users.noreply.github.com>
2023-12-21 17:28:23 +0300

    1st commit

---
+++ alembic/versions/7a7233cca313_1st_migration.py
@@ -0,0 +1,24 @@
+"""1st migration
+
+Revision ID: 7a7233cca313
+Revises: 
+Create Date: 2023-12-21 17:28:14.880969
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7a7233cca313'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
